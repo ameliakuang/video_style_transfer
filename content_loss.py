bb340e2c32c39fd0f73a34301f363a73461539df
Sirui <ariel.chensr@gmail.com>
2025-05-16 21:22:19 -0700

    add content loss

---
+++ content_loss.py
@@ -0,0 +1,47 @@
+import argparse
+import lpips
+import torch
+import os
+from PIL import Image
+import torchvision.transforms as transforms
+
+def load_frames_from_dir(frame_dir):
+    transform = transforms.Compose([
+        transforms.Resize((256, 256)),  # Resize to a fixed size
+        transforms.ToTensor()
+    ])
+    frames = []
+    for filename in sorted(os.listdir(frame_dir)):
+        if filename.endswith(('.jpg', '.png')):
+            img_path = os.path.join(frame_dir, filename)
+            img = Image.open(img_path)
+            tensor = transform(img)
+            frames.append(tensor)
+    return torch.stack(frames)
+
+def get_content_loss(ref_frame, target_frame):
+    loss_fn_alex = lpips.LPIPS(net='alex') # best forward scores
+    # loss_fn_vgg = lpips.LPIPS(net='vgg') # closer to "traditional" perceptual loss, when used for optimization
+
+    d = loss_fn_alex(ref_frame.unsqueeze(0), target_frame.unsqueeze(0))  # Add batch dimension
+    return d.item()  # Convert tensor to Python float
+
+def get_video_content_loss(ref_frames, target_frames):
+    loss = 0
+    for ref_frame, target_frame in zip(ref_frames, target_frames):
+        loss += get_content_loss(ref_frame, target_frame)
+    return loss / len(ref_frames)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ref_frames", type=str, default="video/frames_1778068") # frames_853913, frames_855867
+    parser.add_argument("--target_frames", type=str, default="output/frames_1778068")
+    args = parser.parse_args()
+
+    ref_frames = load_frames_from_dir(args.ref_frames)
+    target_frames = load_frames_from_dir(args.target_frames)
+
+    loss = get_video_content_loss(ref_frames, target_frames)
+    print(f"Content Loss: {loss:.4f}")
+
+
